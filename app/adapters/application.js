04dbb1334192eb33d0b9c719af10ee133bb50350
Patrick <patrick.edward.alcindor@gmail.com>
2019-01-28 00:01:00 -0500

    Revert "creating routes"
    This reverts commit 45f9be49e9bc4ba064b85ea0a27073d7ffc66d8b.

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -3,5 +3,5 @@ import DS from 'ember-data';
 export default DS.JSONAPIAdapter.extend({
 
     namespace: 'v1',
-    host: 'http://localhost:5000'
+    host: 'http://localhost:3000'
 });
--- app/router.js
+++ app/router.js
@@ -8,7 +8,6 @@ const Router = EmberRouter.extend({
 
 Router.map(function() {
   this.route('register-user');
-  this.route('login');
 });
 
 export default Router;
